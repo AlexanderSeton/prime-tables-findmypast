731ca9b0513bc5fa405e04753b3d9e50f920ae2d
Alexander Seton <alexanderseton@protonmail.com>
2022-01-30 19:30:05 +0000

    Minor commit before merge into develop

--- src/models/prime_seive.js
+++ src/models/prime_seive.js
@@ -1,4 +1,4 @@
-// function that takes in a numerical value, then will output an array of primes of the specified length starting from the smallest prime (2) that are contained in this range of natural numbers
+// function that takes in a numerical value, then will output an array of primes (smallest to largest) of the specified length, that are contained in this range of natural numbers
 // function uses the Seive of Eratosthenes algorithm. This is an algorithm that will find all the prime numbers up to (and including) a specified natural number.
 
 function seiveOfEratosthenes(naturalNumbersNeeded, numberPrimesNeeded) {
