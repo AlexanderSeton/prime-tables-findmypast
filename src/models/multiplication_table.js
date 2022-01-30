8dbdc5e9a3ca514ef34a0fb30bf264c55566e845
Alexander Seton <alexanderseton@protonmail.com>
2022-01-30 19:42:38 +0000

    Started creating the unit tests and function for creating a multiplication table

---
+++ src/models/multiplication_table.js
@@ -0,0 +1,7 @@
+// function that generates a matrix (2D array) that will represent the multiplication table
+
+function generateMultiplicationTable(primesArray) {
+    
+}
+
+module.exports = generateMultiplicationTable;
---
+++ src/models/tests/multiplication_table_test.js
@@ -0,0 +1,10 @@
+const assert = require("assert");
+const generateMultiplicationTable = require("../multiplication_table.js");
+
+describe("Multiplication Table Function", function() {
+
+    it("Should output a multiplication table matrix (2D array) for the primes 2, 3 and 5", function() {
+        
+    });
+
+});
