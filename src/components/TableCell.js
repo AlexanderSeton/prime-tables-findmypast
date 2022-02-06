aba76cf68720609a1a1275284f6a492752fadd42
Alexander Seton <alexanderseton@protonmail.com>
2022-02-06 18:59:52 +0000

    Added some further styling, and instructions for the tool tips

--- src/components/TableCell.js
+++ src/components/TableCell.js
@@ -4,10 +4,10 @@ import { Button, Popup } from "semantic-ui-react";
 
 export default function TableElement({ element, rowIndex, cellIndex, getPrimeFactors }) {
     return(
-        <td className="table-cell">
+        <td className="table-cell" style={ rowIndex == cellIndex ? { backgroundColor: "lightblue" } : null }>
             {element}
             {rowIndex != 0 && cellIndex != 0 ?
-                <Popup trigger={<Button className="info-button">?</Button>} position="top center">
+                <Popup trigger={<Button className="info-button">?</Button>} position="right center">
                     <p className="pop-up-text">
                         {getPrimeFactors(rowIndex, cellIndex)[0] + " x " + getPrimeFactors(rowIndex, cellIndex)[1]}
                     </p>
